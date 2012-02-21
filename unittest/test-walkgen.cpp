f7609594304398a64d070c48988f14177e420e1e
Andrei Herdt <andrei.herdt@inria.fr>
2012-02-21 15:51:36 +0100

    Minor homogenisation of test-walkgen

--- unittest/test-walkgen.cpp
+++ unittest/test-walkgen.cpp
@@ -1,5 +1,5 @@
 /*
-*/
+ */
 
 
 
@@ -25,7 +25,6 @@ bool checkFiles(std::ifstream & f1, std::ifstream & f2);
 int copyFile(const std::string & source, const std::string & destination);
 
 int main () {
-
 	// Logging:
 	// --------
 	const int nbFile=8;
@@ -42,7 +41,7 @@ int main () {
 
 	std::vector<std::ofstream*> data_vec(nbFile);
 	std::vector<std::ifstream*> ref_vec(nbFile);
-	for(int i=0;i<nbFile;++i){
+	for (int i = 0; i < nbFile; ++i) {
 		data_vec[i] = new std::ofstream((name_vec[i]+".data").c_str());
 		ref_vec[i] = new std::ifstream((name_vec[i]+".ref").c_str());
 	}
@@ -141,8 +140,8 @@ int main () {
 	return (success)?0:1;
 }
 
-void dumpTrajectory(MPCSolution & result, std::vector<std::ofstream*> & data_vec){
-	for(int i=0;i<result.state_vec[0].CoMTrajX_.rows();++i){
+void dumpTrajectory(MPCSolution & result, std::vector<std::ofstream*> & data_vec) {
+	for (int i = 0; i < result.state_vec[0].CoMTrajX_.rows(); ++i) {
 		*data_vec[0] << result.state_vec[0].CoMTrajX_(i) << " " << result.state_vec[1].CoMTrajX_(i) << " " << result.state_vec[2].CoMTrajX_(i) << std::endl;
 		*data_vec[1] << result.state_vec[0].CoMTrajY_(i) << " " << result.state_vec[1].CoMTrajY_(i) << " " << result.state_vec[2].CoMTrajY_(i) << std::endl;
 		*data_vec[2] << result.CoPTrajX(i)  << std::endl;
@@ -154,8 +153,7 @@ void dumpTrajectory(MPCSolution & result, std::vector<std::ofstream*> & data_vec
 	}
 }
 
-void makeScilabFile(std::string type){
-
+void makeScilabFile(std::string type) {
 	std::ofstream sci(("plot"+type+".sci").c_str());
 	sci << "X=read('CoM_X." << type << "',-1,3);" << std::endl;
 	sci << "Y=read('CoM_Y." << type << "',-1,3);" << std::endl;
@@ -210,30 +208,27 @@ void makeScilabFile(std::string type){
 	sci.close();
 }
 
-bool checkFiles(std::ifstream & fich1, std::ifstream & fich2){
+bool checkFiles(std::ifstream & fich1, std::ifstream & fich2) {
 	bool equal=1;
-	if (fich1 && fich2 )
-	{
-	 std::string lignef1;
-	 std::string lignef2;
-	 while ( std::getline( fich1, lignef1) && std::getline( fich2, lignef2) && equal)
-	 {
-	  if (strcmp(lignef1.c_str(),lignef2.c_str())!=0)
-	  {
-	   equal = 0;
-	  }
-	 }
+	if (fich1 && fich2) {
+		std::string lignef1;
+		std::string lignef2;
+		while (std::getline( fich1, lignef1) && std::getline( fich2, lignef2) && equal) {
+			if (strcmp(lignef1.c_str(),lignef2.c_str())!=0) {
+				equal = 0;
+			}
+		}
 	}
+
 	return equal;
 }
 
-int copyFile(const std::string & source, const std::string & destination)
-{
+int copyFile(const std::string & source, const std::string & destination) {
 	std::ifstream ifs(source.c_str(), std::ios::binary);
 	std::ofstream ofs(destination.c_str(), std::ios::binary);
 	ofs << ifs.rdbuf();
 	ofs.close();
 	ifs.close();
 
-    return 0;
+	return 0;
 }
