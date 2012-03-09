cbd187426bf12c64f7ccaeda0a695b58557eef40
Jory-Lafaye <jlafaye@aldebaran-robotic.com>
2012-03-09 11:04:20 +0100

    Modify Cmake files according to qi-build compilation

---
+++ modules/FindLSSOL.cmake
@@ -0,0 +1,10 @@
+## Copyright (C) 2012 Aldebaran Robotics
+
+clean(LSSOL)
+fpath(LSSOL "lssol/lssol.h")
+flib(LSSOL "liblssol_c.so")
+flib(LSSOL "liblssol.so")
+flib(LSSOL "libblas_c.so")
+flib(LSSOL "libf2c.a")
+export_header(LSSOL)
+export_lib(LSSOL)
