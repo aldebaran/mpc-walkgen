7f195aa55e351be0051997352c41a7216287a82a
Keith Francois <francois.keith@lirmm.fr>
2012-03-19 16:46:03 +0900

    Merge the two cmake files system for the folder unittest.
    The two systems correspond to the one using qibuild and the default one.

---
+++ unittest/SourcesLib.cmake
@@ -0,0 +1,67 @@
+INCLUDE_DIRECTORIES(BEFORE ${PROJECT_SOURCE_DIR}/include-private)
+INCLUDE_DIRECTORIES(BEFORE ${PROJECT_SOURCE_DIR}/src)
+
+## test the rotation of the cholesky matrix
+set(test-rotation_SRC 
+	test-rotation.cpp
+	../include-private/mpc-walkgen/tools.h
+	../src/walkgen/tools.cpp
+	tools-rotation.h
+	tools-rotation.cpp
+)
+
+## Testing the solvers
+# test the lssol solver
+set(test-lssol_SRC test-lssol.cpp)
+
+
+# test the qpoases solver
+set(test-qpoases_SRC test-qpoases.cpp)
+
+## Testing all the solvers
+set(test-qpsolver_SRC 
+	test-qpsolver.cpp
+	../src/walkgen/qp-solver.cpp
+ 	../src/walkgen/qp-matrix.cpp
+ 	../src/walkgen/convex-hull.cpp
+ 	../src/sharedpgtypes.cpp
+)
+
+# 
+set(test-all-solvers_SRC 
+        test-all-solvers.cpp
+        ../src/walkgen/qp-solver.cpp
+        ../src/walkgen/qp-solvers/lssol-solver.cpp
+        ../src/walkgen/qp-solvers/qpoases-solver.cpp
+        ../src/walkgen/qp-matrix.cpp
+        ../src/walkgen/convex-hull.cpp
+        ../src/sharedpgtypes.cpp
+)
+
+# 
+set(bench-qpsolver_SRC 
+  bench-qpsolver.cpp
+  ../src/walkgen/qp-solver.cpp
+  ../src/walkgen/qp-matrix.cpp
+  ../src/walkgen/convex-hull.cpp
+  ../src/walkgen/mpc-debug.cpp
+  ../src/sharedpgtypes.cpp
+)
+
+if(LSSOL_FOUND)
+  list(APPEND bench-qpsolver_SRC   ../src/walkgen/qp-solvers/lssol-solver.cpp)
+  list(APPEND test-all-solvers_SRC    ../src/walkgen/qp-solvers/lssol-solver.cpp)
+  list(APPEND test-qpsolver_SRC    ../src/walkgen/qp-solvers/lssol-solver.cpp)
+endif(LSSOL_FOUND)
+
+if(QPOASES_FOUND)
+  list(APPEND bench-qpsolver_SRC   ../src/walkgen/qp-solvers/qpoases-solver.cpp)
+  list(APPEND test-all-solvers_SRC   ../src/walkgen/qp-solvers/qpoases-solver.cpp)
+  list(APPEND test-qpsolver_SRC    ../src/walkgen/qp-solvers/qpoases-solver.cpp)
+endif(QPOASES_FOUND)
+
+# tests of the walkgen
+set(test-walkgen_SRC  test-walkgen.cpp)
+set(bench-solvers_SRC  bench-solvers.cpp)
+
+
