05e09d39125ab5ebc252a0c9604cd2829e480c92
Keith Francois <francois.keith@lirmm.fr>
2012-03-20 00:17:57 +0900

    Add missing cmake file.

---
+++ src/SourcesLib.cmake
@@ -0,0 +1,40 @@
+SET(mpc-walkgen_SRC
+ sharedpgtypes.cpp 
+ 
+ walkgen/convex-hull.cpp 
+ walkgen/orientations-preview.cpp 
+
+ walkgen/qp-solver.cpp
+ 
+ walkgen/state-solver.cpp
+ walkgen/state-solvers/fsm-solver.cpp 
+ 
+ walkgen/rigid-body-system.cpp
+ walkgen/rigid-body.cpp
+ walkgen/rigid-bodies/com-body.cpp
+ walkgen/rigid-bodies/foot-body.cpp
+ 
+ walkgen/qp-matrix.cpp
+ walkgen/qp-generator.cpp
+ walkgen/qp-preview.cpp
+ 
+ walkgen/interpolation.cpp
+ walkgen/tools.cpp
+ walkgen/types.cpp
+ walkgen/walkgen.cpp
+ 
+ walkgen/mpc-debug.cpp
+)
+
+if(LSSOL_FOUND)
+  list(APPEND mpc-walkgen_SRC
+    walkgen/qp-solvers/lssol-solver.cpp
+  )
+endif(LSSOL_FOUND)
+
+if(QPOASES_FOUND)
+  list(APPEND mpc-walkgen_SRC
+    walkgen/qp-solvers/qpoases-solver.cpp
+  )
+endif(QPOASES_FOUND)
+
