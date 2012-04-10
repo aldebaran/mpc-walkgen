ccdbc113e02a0cd210e2c933ca2cb1448f72e7d5
Jory-Lafaye <jlafaye@aldebaran-robotics.com>
2012-04-10 12:19:30 +0200

    Add lssol-config.cmake for qibuild

---
+++ modules/lssol-config.cmake
@@ -0,0 +1,16 @@
+## Copyright (C) 2012 Aldebaran Robotics
+
+clean(LSSOL)
+fpath(LSSOL "lssol/lssol.h")
+flib(LSSOL "liblssol_c.so")
+flib(LSSOL "liblssol.so")
+
+flib(LSSOL "libblas_c.so")
+
+flib(LSSOL "libcblas.so" PATH_SUFFIXES "blas/reference")
+flib(LSSOL "libblas.so" PATH_SUFFIXES "blas/reference")
+
+flib(LSSOL "libf2c.a")
+export_header(LSSOL)
+export_lib(LSSOL)
+
