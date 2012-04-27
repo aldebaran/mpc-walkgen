e858632040e5273044aa971b4015291249370634
Keith Francois <francois.keith@lirmm.fr>
2012-04-27 16:38:03 +0200

    Unify indentation.

--- src/sharedpgtypes-humanoid.cpp
+++ src/sharedpgtypes-humanoid.cpp
@@ -5,16 +5,16 @@
 namespace MPCWalkgen
 {
 
-MPCSolution::MPCSolution()
-:useWarmStart(true)
-,state_vec(3)
-{}
+  MPCSolution::MPCSolution()
+  :useWarmStart(true)
+  ,state_vec(3)
+  {}
 
-void MPCSolution::reset(){
-	supportStates_vec.resize(0);
-	supportOrientations_vec.resize(0);
-	supportTrunkOrientations_vec.resize(0);
-}
+  void MPCSolution::reset(){
+    supportStates_vec.resize(0);
+    supportOrientations_vec.resize(0);
+    supportTrunkOrientations_vec.resize(0);
+  }
 
   MPCData::MPCData()
   :QPSamplingPeriod(0.1)
@@ -28,15 +28,15 @@ void MPCSolution::reset(){
   {}
 
   int MPCData::nbFeedbackSamplesLeft(double firstIterationduration) const{
-          return static_cast<int> (round(firstIterationduration / MPCSamplingPeriod)-1 );
+    return static_cast<int> (round(firstIterationduration / MPCSamplingPeriod)-1 );
   }
 
   int MPCData::nbFeedbackSamplesStandard() const{
-          return static_cast<int> (round(QPSamplingPeriod / MPCSamplingPeriod) );
+    return static_cast<int> (round(QPSamplingPeriod / MPCSamplingPeriod) );
   }
 
   int MPCData::nbSamplesControl() const{
-          return static_cast<int> (round(MPCSamplingPeriod / actuationSamplingPeriod) );
+    return static_cast<int> (round(MPCSamplingPeriod / actuationSamplingPeriod) );
   }
 
 
@@ -45,22 +45,22 @@ void MPCSolution::reset(){
         const HipYawData &leftHipYaw, const HipYawData &rightHipYaw,
         double mass)
   :	CoMHeight(0.814)
-        ,freeFlyingFootMaxHeight(0.05)
-        ,leftFoot(leftFoot)
-        ,rightFoot(rightFoot)
-        ,leftHipYaw(leftHipYaw)
-        ,rightHipYaw(rightHipYaw)
-        ,robotMass(mass)
-        ,leftFootPos()
-        ,rightFootPos()
-        ,leftFootHull()
-        ,rightFootHull()
-        ,CoPLeftSSHull()
-        ,CoPRightSSHull()
-        ,CoPLeftDSHull()
-        ,CoPRightDSHull() {
-          leftFootPos << 0.00949035, 0.095, 0;
-          rightFootPos << 0.00949035, -0.095, 0;
+  ,freeFlyingFootMaxHeight(0.05)
+  ,leftFoot(leftFoot)
+  ,rightFoot(rightFoot)
+  ,leftHipYaw(leftHipYaw)
+  ,rightHipYaw(rightHipYaw)
+  ,robotMass(mass)
+  ,leftFootPos()
+  ,rightFootPos()
+  ,leftFootHull()
+  ,rightFootHull()
+  ,CoPLeftSSHull()
+  ,CoPRightSSHull()
+  ,CoPLeftDSHull()
+  ,CoPRightDSHull() {
+    leftFootPos << 0.00949035, 0.095, 0;
+    rightFootPos << 0.00949035, -0.095, 0;
   }
   RobotData::RobotData(){}
 
@@ -68,24 +68,24 @@ void MPCSolution::reset(){
   :instantVelocity(nb)
   ,CopCentering(nb)
   ,JerkMin(nb) {
-          CopCentering[0]    = 0.0001;
-          JerkMin[0]         = 0.001;
-          instantVelocity[0] = 1;
+    CopCentering[0]    = 0.0001;
+    JerkMin[0]         = 0.001;
+    instantVelocity[0] = 1;
 
-	  CopCentering[1]    = 10;
-	  JerkMin[1]         = 0.001;
-	  instantVelocity[1] = 1;
+    CopCentering[1]    = 10;
+    JerkMin[1]         = 0.001;
+    instantVelocity[1] = 1;
 
-          activePonderation = 1;
+    activePonderation = 1;
   }
 
 
   HipYawData::HipYawData()
-  : lowerBound(-0.523599)
-  , upperBound(0.785398)
-  , lowerVelocityBound(-3.54108)
-  , upperVelocityBound(3.54108)
-  , lowerAccelerationBound(-0.1)
-  , upperAccelerationBound(0.1) {
-  }
+  :lowerBound(-0.523599)
+  ,upperBound(0.785398)
+  ,lowerVelocityBound(-3.54108)
+  ,upperVelocityBound(3.54108)
+  ,lowerAccelerationBound(-0.1)
+  ,upperAccelerationBound(0.1)
+  {}
 }
