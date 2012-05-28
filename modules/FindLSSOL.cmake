e9d40e1cc7baaaece3d4e0a411d82f29a5157b92
Sebastien Barthelemy <sbarthelemy@aldebaran-robotics.com>
2012-05-28 17:28:29 +0200

    Revert "FindLSSOL: fix LSSOL segfault due to wrong link order"
    This reverts commit de9d174b2f9750c6d300e5d922a70de91656aa59.
    
    This commit restores the previous link order. We had a problem
    with it on ia32, but it was most probably the result of a flacky
    LSSOL build.
    
    Now, with a freshly rebuilt LSSOL (as a static library), and the
    restored link order, it works on both 32 and 64 bits versions of linux.
    
    So with this commit
    
       ia32, static libs: ok
       x64, static libs: ok
    
    Note that the LSSOL build uses both f2c and blas from the ubuntu
    packages, not the ones included in LSSOL distribution.

--- modules/FindLSSOL.cmake
+++ modules/FindLSSOL.cmake
@@ -9,10 +9,10 @@ endif()
 if(NOT ${uprefix}_FOUND)
   # if not found yet, look at standard places
   find_path(${uprefix}_INCLUDE_DIRS "lssol/lssol.h")
-  find_library(lssol_c_LIB "lssol_c")
   find_library(lssol_LIB "lssol")
-  find_library(blas_LIB "blas")
+  find_library(lssol_c_LIB "lssol_c")
   find_library(f2c_LIB "f2c")
+  find_library(blas_LIB "blas" NAMES "blas_c")
   #find_library(${uprefix}_GFORTRAN_LIBRARY "gfortran" PATH_SUFFIXES
   #    "gcc/i686-linux-gnu/4.6" "gcc/x86_64-linux-gnu/4.6")
 endif()
@@ -21,7 +21,7 @@ endif()
 # If not found, the compilation flags and the #include<> may be wrong.
 find_package_handle_standard_args(${prefix}
     REQUIRED_VARS ${uprefix}_INCLUDE_DIRS
-    lssol_c_LIB lssol_LIB blas_LIB f2c_LIB
+    lssol_LIB lssol_c_LIB f2c_LIB blas_LIB
     VERSION_VAR ${uprefix}_VERSION)
 
 # publish
@@ -30,9 +30,8 @@ if(${uprefix}_FOUND)
   # gfortran cannot be found by find_library as is lies in
   # /usr/lib/gcc/<arch>/<gcc_version>/
   # So we let the linker find it.
-  # Also, beware the *link order matters*
   set("${uprefix}_LIBRARIES"
-      ${lssol_c_LIB} ${lssol_LIB} ${blas_LIB} ${f2c_LIB}
+      ${lssol_LIB} ${lssol_c_LIB} ${f2c_LIB} ${blas_LIB}
       gfortran
       CACHE INTERNAL "" FORCE)
 
